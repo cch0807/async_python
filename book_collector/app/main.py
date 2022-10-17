23dd625c4bb96e383f8c653c59609d4a51ce04cd
cch0807 <ckdgus11021@gmail.com>
2022-10-18 00:58:49 +0900

    20221018_1

--- book_collector/app/main.py
+++ book_collector/app/main.py
@@ -2,12 +2,19 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+print(BASE_DIR)
 
 app = FastAPI()
-
-templates = Jinja2Templates(directory="book_collector/app/templates")
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 
 @app.get("/items/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("./item.html", {"request": request, "id": id})
+    print(request.headers)
+    return templates.TemplateResponse(
+        "./item.html", {"request": request, "id": id, "data": "hello fastapi"}
+    )
