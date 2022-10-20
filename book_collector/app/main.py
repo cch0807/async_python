00d7722284f1327240abb9a7a79b87cb33182b55
cch0807 <ckdgus11021@gmail.com>
2022-10-21 01:06:39 +0900

    20221021_1

--- book_collector/app/main.py
+++ book_collector/app/main.py
@@ -4,6 +4,9 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
+from ..models import mongodb
+
+
 BASE_DIR = Path(__file__).resolve().parent
 
 print(BASE_DIR)
@@ -25,3 +28,16 @@ async def search(request: Request, q: str):
     return templates.TemplateResponse(
         "./index.html", {"request": request, "title": "Collector", "keyword": q}
     )
+
+
+@app.on_event("startup")
+def on_app_start():
+    print("start server")
+    """before app starts"""
+    mongodb.connect()
+
+
+@app.on_event("shutdown")
+async def on_app_shutdown():
+    # await mongodb.close()
+    print("end server")
